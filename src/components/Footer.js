84c5d8cd1cb7f97787612ac80944c0aa242ad50f
Evania Torres <106711685+Sterling2Archer@users.noreply.github.com>
2023-01-17 19:06:02 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,15 +5,11 @@ import {FaTwitter, FaYoutube} from 'react-icons/fa';
 const Footer = () => {
   return (
     <div className='footer'>
-          <div className='center'>
-          <div className='SNS'>
-              <h4><center>FOLLOW @BLACKPINK ON </center></h4>
-              <a href='https://twitter.com/blackpink'><FaTwitter size={70} style={{ color: '#FFB6C1', marginLeft: '7rem' }} /> </a>
-              <a href='https://www.youtube.com/@BLACKPINK'><FaYoutube size={70} style={{ color: '#FFB6C1', marginRight: '0rem' }} /> </a>
-          </div>
-          </div>
+              <h4>FOLLOW BLACKPINK</h4>
+              <a href='https://twitter.com/BLACKPINK'><FaTwitter size={50} style={{ color: '#FFB6C1'}}/></a>
+              <a href='https://www.youtube.com/@BLACKPINK'><FaYoutube size={50} style={{ color: '#FFB6C1'}}/></a>
     </div>    
   )
-}
+};
 
 export default Footer;
